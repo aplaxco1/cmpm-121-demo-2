cdf3a429559589b5a8a3cbdf4eddb597cb856b73
aplaxco1 <aplaxco@ucsc.edu>
2023-10-13 17:58:13 -0700

    testing deployment

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Autumn's Sketch Pad";
 
 document.title = gameName;
 
