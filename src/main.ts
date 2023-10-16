392c241451458b0aed668318fafb00beceaa9417
aplaxco1 <aplaxco@ucsc.edu>
2023-10-15 17:53:51 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,30 @@ clearButton.addEventListener("click", () => {
   canvas.dispatchEvent(drawingChangedEvent);
 });
 
+// undo button
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+app.append(undoButton);
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    redoLines.push(lines.pop()!);
+    canvas.dispatchEvent(drawingChangedEvent);
+  }
+});
+
+// redo button
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "redo";
+app.append(redoButton);
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    lines.push(redoLines.pop()!);
+    canvas.dispatchEvent(drawingChangedEvent);
+  }
+});
+
 // div to seperate elements
 const div = document.createElement("div");
 div.innerHTML = `<br>`;
@@ -31,6 +55,7 @@ const canvas = document.createElement("canvas");
 canvas.id = "canvas";
 app.append(canvas);
 
+// ---- DRAWING STUFF ----//
 const ctx = canvas.getContext("2d");
 const rect = canvas.getBoundingClientRect();
 
@@ -50,6 +75,7 @@ const cursor = { active: false, x: 0, y: 0 };
 // list of lines to be drawn each "drawing-changed" event
 const lines: { x: number; y: number }[][] = [];
 let currLine: { x: number; y: number }[] | null = null;
+const redoLines: { x: number; y: number }[][] = [];
 
 // redraws canvas on drawing changed event
 const drawingChangedEvent = new Event("drawing-changed");
@@ -80,6 +106,7 @@ canvas.addEventListener("mousedown", (e) => {
   cursor.y = getMouseY(canvas, e);
   currLine = [];
   lines.push(currLine);
+  redoLines.splice(0, redoLines.length);
   currLine.push({ x: cursor.x, y: cursor.y });
 
   canvas.dispatchEvent(drawingChangedEvent);
